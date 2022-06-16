9c31fe3d8e480a35e63a64606651c1b6be5ebcbf
Akhil-Sudhakaran <akhil1101@gmail.com>
2022-06-16 10:11:18 +0530

    initial commit

---
+++ src/main/java/com/innovature/test/maven_lib/Test.java
@@ -0,0 +1,10 @@
+package com.innovature.test.maven_lib;
+
+import com.innovature.repo.calc.Calc;
+
+
+public class Test {
+    public static void main(String args[]) {
+        System.out.println(Calc.add(5, 6));
+    }
+}
